db453d27412b06cfcf54ec0ecfa45741e4c53446
PRATYAY CHATTERJEE <pratyay.ch2006@gmail.com>
2026-08-27 23:14:57 +0530

    ddddd

--- Frontend/js/shop.js
+++ Frontend/js/shop.js
@@ -64,7 +64,7 @@ const craftCategories = {
                  name: "Woodcraft",
                 image: "assets/Artworks/art1.png",
                 category: "55",
-                price: "₹554",
+                price: "₹55",
                 link: "terracotta-vase.html"
             },
             {
